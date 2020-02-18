13aaf6c2f9734de54557491f1c7db20ec89df7ef
Marvin POLOMACK <marvin.polomack@orange.com>
2020-02-18 16:56:35 +0100

    Adding Fuzzy Finder (regex search)

--- kube-selector.py
+++ kube-selector.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 import os
 import re
 import sys
+from iterfzf import iterfzf
 
 conffiles = []
 i = 1
@@ -16,7 +17,7 @@ try:
 except OSError as error:
     if error.errno == 22:
         print(
-            "[ERROR] Current config file is not a symlink. Aborting to avoid current config lost.")
+            "[ERROR] Current config file is not a symlink. Aborting to avoid current config loss.")
         sys.exit(1)
     if error.errno == 2:
         print("[WARN] Active config file not found")
@@ -28,18 +29,20 @@ for conffile in os.listdir(CONF_PATH):
 
 conffiles.sort()
 
+# Put current config file in first place
 for conffile in conffiles:
-        if os.path.join(CONF_PATH, conffile) == current:
-            indice = "> %i" % i
-        else:
-            indice = "  %i" % i
-        print("%s: %s" % (indice, conffile.replace('config-', '', 1)))
-        i = i + 1
+    if os.path.join(CONF_PATH, conffile) == current:
+        conffiles.remove(conffile)
+        conffiles.insert(0, conffile)
 
 try:
-    val = int(input("Selection (Ctrl+C to cancel): "))
+    choice = iterfzf(conffiles)
+    if choice == None:
+        raise KeyboardInterrupt()
+    else:
+        val = choice
 except KeyboardInterrupt:
-    print("\n[INFO] Nothing changed. Bye, bye !")
+    print("\n[INFO] Nothing changed. Your k8s config file is still %s Bye, bye !" % conffiles[0])
     sys.exit(0)
 
 # Try to remove existing config file if exists
@@ -48,7 +51,7 @@ try:
 except OSError:
     print("[WARN] Active config file was not found")
 
-src = os.path.join(CONF_PATH, conffiles[val - 1])
+src = os.path.join(CONF_PATH, val)
 os.symlink(src, dst)
 
-print("[OK] Your k8s config file is now '%s'" % conffiles[val - 1])
+print("[OK] Your k8s config file is now '%s'" % val)
